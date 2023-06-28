0bb58436ceb3a428c7b0b6c3fdf5e43a3d1f5f6b
ANastya28 <guselnikova2803@gmail.com>
2023-06-28 23:10:31 +0300

    Done Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.body;
+
+
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+btnStop.setAttribute('disabled', true);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function makeBodyColorChange() {
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+function onBtnStartClick() {
+    makeBodyColorChange();
+
+    btnStop.removeAttribute('disabled');
+    btnStart.setAttribute('disabled', true);
+};
+
+function onBtnStopClick() {
+    clearInterval(timerId);
+
+    btnStop.setAttribute('disabled', true);
+    btnStart.removeAttribute('disabled');
+};
