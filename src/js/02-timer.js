716ba8bc08fa45d67ca3cea068e7dcea776efe79
ANastya28 <guselnikova2803@gmail.com>
2023-06-29 17:12:32 +0300

    Done Task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,19 +2,62 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
+const dateInput = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+btnStart.setAttribute('disabled', true);
+let chosenDate = null;
+let timerId = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+        Notiflix.Notify.warning("Please choose a date in the future");
+        btnStart.setAttribute('disabled', true);
+    } else {
+        chosenDate = selectedDates[0];
+        btnStart.removeAttribute('disabled');
+        btnStart.addEventListener('click', onBtnStart);
+    }
   },
 };
 
+
 flatpickr("#datetime-picker", options);
 
-Notiflix.Notify.warning("Please choose a date in the future");
+function onBtnStart(){
+  timerId = setInterval(() => {
+  btnStart.setAttribute('disabled', true);
+  dateInput.setAttribute('disabled', true);
+
+  const currentTime = Date.now();
+  const deltaTime = chosenDate - currentTime;
+
+  if (deltaTime < 1000) {
+    clearInterval(timerId);
+    btnStart.removeAttribute('disabled');
+    dateInput.removeAttribute('disabled');
+  }
+ 
+  const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    timerInterface({ days, hours, minutes, seconds });
+  }, 1000)
+}
+
+function timerInterface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minutesEl.textContent = minutes;
+  secondsEl.textContent = seconds;
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -35,6 +78,10 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
